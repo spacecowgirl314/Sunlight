8e57d93928389d5f44837a280d4fd1e7ea31d9c6
Chloe Stars <hachidorii@icloud.com>
2013-02-08 02:32:08 -0800

    Made anything that uses PSCGradientView able to drag the window.

--- Sunlight/PSCGradientView.m
+++ Sunlight/PSCGradientView.m
@@ -28,6 +28,36 @@
 	return self;
 }
 
+// from http://stackoverflow.com/a/4564630/1000339
+-(void)mouseDown:(NSEvent *)theEvent {
+    NSRect  windowFrame = [[self window] frame];
+	
+    initialLocation = [NSEvent mouseLocation];
+	
+    initialLocation.x -= windowFrame.origin.x;
+    initialLocation.y -= windowFrame.origin.y;
+}
+
+- (void)mouseDragged:(NSEvent *)theEvent {
+    NSPoint currentLocation;
+    NSPoint newOrigin;
+	
+    NSRect  screenFrame = [[NSScreen mainScreen] frame];
+    NSRect  windowFrame = [self frame];
+	
+    currentLocation = [NSEvent mouseLocation];
+    newOrigin.x = currentLocation.x - initialLocation.x;
+    newOrigin.y = currentLocation.y - initialLocation.y;
+	
+    // Don't let window get dragged up under the menu bar
+    if( (newOrigin.y+windowFrame.size.height) > (screenFrame.origin.y+screenFrame.size.height) ){
+        newOrigin.y=screenFrame.origin.y + (screenFrame.size.height-windowFrame.size.height);
+    }
+	
+    //go ahead and move the window to the new location
+    [[self window] setFrameOrigin:newOrigin];
+}
+
 - (void)drawRect:(NSRect)rect {
 	if (endingColor == nil || [startingColor isEqual:endingColor]) {
 		// Fill view with a standard background color
