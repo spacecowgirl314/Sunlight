cf792fde28f4c34578d12827433f69714fffe415
Brady Valentino <bradyvalentino@me.com>
2013-03-06 19:17:17 -0800

    Real name colour in the post cell

--- Sunlight/PSCAppDelegate.m
+++ Sunlight/PSCAppDelegate.m
@@ -1351,7 +1351,7 @@
 	[result setPost:post];
 	// set real name
 	NSString *userNameAndUsername = [[NSString alloc] initWithFormat:@"%@ %@", [user name], [[user username] appNetUsernameString]];
-	NSMutableAttributedString *userNameandUsernameAttributedString = [[NSMutableAttributedString alloc] initWithString:userNameAndUsername attributes:@{NSFontAttributeName:[NSFont fontWithName:@"Helvetica Neue Medium" size:13], NSForegroundColorAttributeName:[NSColor colorWithDeviceRed:0.314 green:0.314 blue:0.314 alpha:1.0]}];
+	NSMutableAttributedString *userNameandUsernameAttributedString = [[NSMutableAttributedString alloc] initWithString:userNameAndUsername attributes:@{NSFontAttributeName:[NSFont fontWithName:@"Helvetica Neue Medium" size:13], NSForegroundColorAttributeName:[NSColor colorWithDeviceRed:0.286 green:0.286 blue:0.286 alpha:1.0]}];
 	[userNameandUsernameAttributedString addAttributes:@{NSFontAttributeName:[NSFont fontWithName:@"Helvetica Neue" size:12], NSForegroundColorAttributeName:[NSColor colorWithDeviceRed:0.545 green:0.545 blue:0.545 alpha:1.0]} range:[userNameAndUsername rangeOfString:[[user username] appNetUsernameString]]];
 	[[result userField] setAttributedStringValue:userNameandUsernameAttributedString];
 	// set action button's status, have we starred something?
