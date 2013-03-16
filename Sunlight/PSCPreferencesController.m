a80f3a51b2fc6dcf9252b1548edad852352fd745
Chloe Stars <hachidorii@icloud.com>
2013-03-15 23:53:28 -0700

    Add some of OS's default built-in images to the preferences so that it isn't empty.

--- Sunlight/PSCPreferencesController.m
+++ Sunlight/PSCPreferencesController.m
@@ -42,10 +42,10 @@
 
 - (void)setupToolbar
 {
-	[self addView:generalPreferences label:@"General"];
-	[self addView:accountsPreferences label:@"Accounts"];
-	[self addView:servicesPreferences label:@"Services"];
-	[self addView:notificationsPreferences label:@"Notifications"];
+	[self addView:generalPreferences label:@"General" image:[NSImage imageNamed:@"NSPreferencesGeneral"]];
+	[self addView:accountsPreferences label:@"Accounts" image:[NSImage imageNamed:@"NSUser"]];
+	[self addView:servicesPreferences label:@"Services" image:[NSImage imageNamed:@"NSUserAccounts"]];
+	[self addView:notificationsPreferences label:@"Notifications" image:[NSImage imageNamed:@"NSAdvanced"]];
 }
 
 @end
